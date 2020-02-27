6a7a64253ccf0ebff075fb8a274239557794379b
Asten Fuller <astenhfuller@gmail.com>
2020-02-27 12:48:01 -0800

    yup

---
+++ server/datasources.production.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'db': {
+    'name': 'db',
+    'connector': 'memory',
+  },
+  'MongoDB': {
+    'name': 'MongoDB',
+    'connector': 'mongodb',
+    'url': process.env.MONGODB_URI,
+  },
+};
