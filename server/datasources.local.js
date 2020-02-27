2f6c5d69f6b2c1230ad6cdd44e301f13e517115f
Asten Fuller <astenhfuller@gmail.com>
2020-02-27 12:49:39 -0800

    Circleci

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -6,7 +6,7 @@ module.exports = {
     "MongoDB": {
       "host": "",
       "port": 0,
-      "url": "",
+      "url": "mongodb://localhost/loopback-workshop-dev",
       "database": "loopback-workshop-dev",
       "password": "",
       "name": "MongoDB",
